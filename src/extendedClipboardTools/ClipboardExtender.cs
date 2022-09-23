54d59b87339146f8cb06bf7381ae844d9e62d85c
lordhollow <hollowscircus@gmail.com>
2022-09-23 19:03:00 +0900

    generate clipboard tool from define sheet

--- src/extendedClipboardTools/ClipboardExtender.cs
+++ src/extendedClipboardTools/ClipboardExtender.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
 
 namespace extendedClipboardTools
 {
@@ -12,25 +14,68 @@ namespace extendedClipboardTools
     /// </summary>
     class ClipboardExtender
     {
+        /// <summary>
+        /// Singleton Object
+        /// </summary>
         public static readonly ClipboardExtender Instance = new ClipboardExtender();
 
+        /// <summary>
+        /// loaded tools
+        /// </summary>
         private List<IClipboardTool> tools;
 
-        public ClipboardExtender()
-        {
-            tools = new List<IClipboardTool>
-            {
-                new SampleNormalTool { Enable = true },
-                new SampleContinuousTool { Enable = true }
-            };
+        /// <summary>
+        /// loaded assemblies
+        /// </summary>
+        private List<Assembly> assemblies = new List<Assembly>();
 
-            foreach(var tool in tools)
+        /// <summary>
+        /// [private] constructor
+        /// </summary>
+        private ClipboardExtender()
+        {
+            tools = new List<IClipboardTool>();
+
+            LoadExternalTools();
+
+            foreach (var tool in tools)
             {
                 tool.Prepare();
             }
         }
 
+        /// <summary>
+        /// Loaded Tools
+        /// </summary>
         public IEnumerable<IClipboardTool> Tools { get { return tools; } }
+
+        /// <summary>
+        /// load clipboard tools from define sheet(tool/*.xml)
+        /// </summary>
+        private void LoadExternalTools()
+        {
+            //enumerate define sheets
+            var files = new List<ExtendToolDefineSheet>();
+            var path = Path.Combine(PathExtension.ApplicationPath, "tool");
+            foreach(var file in Directory.GetFiles(path, "*.xml"))
+            {
+                var def = ExtendToolDefineSheet.CreateFromFile(file);
+                if (def != null)
+                {
+                    files.Add(def);
+                }
+            }
+
+            //create assembly
+            var compiler = new ClipboardToolCompiler();
+            if (compiler.CreateTools(files))
+            {
+                //get clipboard tools in assembly
+                assemblies.Add(compiler.Assembly);
+                tools.AddRange(compiler.ClipboardTools);
+            }
+
+        }
 
     }
 }
