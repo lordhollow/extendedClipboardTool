bd0ec64ac9dd25c502d0e93c9e78eef590c96acc
lordhollow <hollowscircus@gmail.com>
2022-09-23 13:54:41 +0900

    add tool base class

---
+++ src/extendedClipboardTools/ClibboardToolBase.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace extendedClipboardTools
+{
+    public abstract class ClipboardTool : IClipboardTool
+    {
+        #region IClipboardTool
+        public string Name { get; protected set; }
+        public string Description { get; protected set; }
+        public bool Continuous { get; protected set; }
+        public bool Enable { get; set; }
+        public virtual void Prepare() { }
+        public virtual bool Checker(string opt) { return false; }
+        public virtual void Action(string opt) { }
+        public virtual void FinishAction(string opt) { }
+        #endregion
+
+        protected Regex regex;
+
+        protected object value;
+
+        protected string StringData
+        {
+            get
+            {
+                if (Clipboard.ContainsText())
+                {
+                    return Clipboard.GetText();
+                }
+                return null;
+            }
+        }
+
+        protected void Write(string s)
+        {
+            Clipboard.SetText(s);
+        }
+    }
+
+    public class SampleNormalTool : ClipboardTool
+    {
+        public SampleNormalTool()
+        {
+            Name = "StripQuote";
+            Description = "get quoted string list without quote.";
+            Continuous = false;
+        }
+
+        public override void Prepare()
+        {
+            regex = new Regex(@"\""([^\""]+)\""");
+        }
+
+        public override bool Checker(string opt)
+        {
+            var s = StringData;
+            if (!string.IsNullOrEmpty(s))
+            {
+                var m = regex.Match(s);
+                if (m.Success)
+                {
+                    value = m.Groups[1].ToString();
+                    return false;
+                }
+            }
+            return false;
+        }
+
+        public override void Action(string opt)
+        {
+            Write(value as string);
+        }
+    }
+
+    public class SampleContinuousTool : ClipboardTool
+    {
+        public SampleContinuousTool()
+        {
+            Name = "Concat$NewLine$Tab";
+            Description = "Concatenate with linebreak or tab";
+            Continuous = true;
+        }
+
+        public override bool Checker(string opt)
+        {
+            return !string.IsNullOrEmpty(StringData);
+        }
+
+        public override void Action(string opt)
+        {
+            if (value == null)
+            {
+                value = StringData;
+            }
+            else
+            {
+                string sep = "";
+                if (opt == "NewLine")
+                {
+                    sep = Environment.NewLine;
+                }
+                else if (opt == "Tab")
+                {
+                    sep = "\t";
+                }
+                value += sep + StringData;
+            }
+        }
+
+        public override void FinishAction(string opt)
+        {
+            Write(value as string);
+            value = null;
+        }
+    }
+
+}
---
+++ src/extendedClipboardTools/IClipboardTool.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace extendedClipboardTools
+{
+    public interface IClipboardTool
+    {
+        string Name { get;  }
+
+        string Description { get; }
+
+        bool Continuous { get; }
+
+        bool Enable { get; set; }
+
+        void Prepare();
+
+        bool Checker(string opt);
+
+        void Action(string opt);
+
+        void FinishAction(string opt);
+    }
+}
