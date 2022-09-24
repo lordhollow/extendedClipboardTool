d55010b06289875515ba70077917b28725710665
lordhollow <hollowscircus@gmail.com>
2022-09-24 21:54:07 +0900

    state of clipboard tool

---
+++ src/extendedClipboardTools/ToolState.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace extendedClipboardTools
+{
+    /// <summary>
+    /// State of clipboard tool.
+    /// </summary>
+    public enum ToolState
+    {
+        /// <summary>
+        /// tool is enable
+        /// </summary>
+        Enable,
+
+        /// <summary>
+        /// tool is disable
+        /// </summary>
+        Disable,
+
+
+        /// <summary>
+        /// tool can not load
+        /// </summary>
+        Error,
+    }
+}
